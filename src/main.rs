2c183aba58514cb35724b16de63329f6011c7756
Jason <JasonVanceMiller@gmail.com>
2024-09-24 16:29:38 -0500

    Using the first chess library I found

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate shakmaty;
+
 fn main() {
     println!("Hello, world!");
+
+    use shakmaty::{Chess, Position};
+
+    let pos = Chess::default();
+    let legals = pos.legal_moves();
+    assert_eq!(legals.len(), 20);
 }

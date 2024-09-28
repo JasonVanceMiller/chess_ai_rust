74181de1ab1ddb56b2bc612dc2375555bdf7c26b
Jason <JasonVanceMiller@gmail.com>
2024-09-28 16:02:37 -0500

    I already hate rust.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 extern crate shakmaty;
 
+use shakmaty::{Chess, Position};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Gamer!");
 
-    use shakmaty::{Chess, Position};
-
-    let pos = Chess::default();
+    let pos : Chess = Chess::default();
+    println!("{}", pos.board());
     let legals = pos.legal_moves();
-    assert_eq!(legals.len(), 20);
+    let result = pos.play(&legals[0]);
+    if let Ok(pos) = result {
+        println!("Played our first move!");
+        println!("{}", pos.board());
+    } else {
+        println!("Failed to play our first move!");
+    }
+    //assert_eq!(legals.len(), 20);
 }
+
